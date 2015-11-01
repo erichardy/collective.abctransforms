abc2d0133e7f39c4864477fcc902ee1bf98d7cb1
Eric Hardy <Eric.Hardy@univ-brest.fr>
2015-11-01 21:47:29 +0100

    other tests

--- src/collective/abctransforms/tests/testUtils.py
+++ src/collective/abctransforms/tests/testUtils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# from collective.abctransforms.testing import \
-#     COLLECTIVE_ABCTRANSFORMS_FUNCTIONAL_TESTING  # noqa
+# tests with the function : from_to
+
 from collective.abctransforms.tests.utils import (input_file_path,
                                                   output_file_path)
 from collective.abctransforms.utils import from_to
@@ -10,7 +10,6 @@ import unittest
 
 class TestAbcToMidi(unittest.TestCase):
 
-    # layer = COLLECTIVE_ABCTRANSFORMS_FUNCTIONAL_TESTING
 
     def setUp(self):
         """Custom shared utility setup for tests."""
@@ -32,3 +31,41 @@ class TestAbcToMidi(unittest.TestCase):
                       toappend='\n\n',
                       logging=False)
         self.assertEqual(got, midi)
+
+    def test_fromMIDI_toAIFF(self):
+        mi = open(output_file_path('DonaldBlue1.mid'), "rb")
+        midi = mi.read()
+        mi.close()
+        Aiff = open(output_file_path('DonaldBlue1.aiff'), "rb")
+        aiff = Aiff.read()
+        Aiff.close()
+        timidity = ["timidity",
+                    '-A 400',
+                    '-EFchorus=2,50',
+                    '-EFreverb=2',
+                    "datain",
+                    '-o', "dataout",
+                    '-Oa']
+        got = from_to(midi,
+                      timidity,
+                      logging=False)
+        self.assertEqual(got, aiff)
+
+    def test_fromAIFF_toMP3(self):
+        Aiff = open(output_file_path('DonaldBlue1.aiff'), "rb")
+        aiff = Aiff.read()
+        Aiff.close()
+        fdMP3 = open(output_file_path('DonaldBlue1.mp3'), "rb")
+        mp3 = fdMP3.read()
+        fdMP3.close()
+        lame = ["lame",
+                '--cbr',
+                '-b 32',
+                '-f',
+                '--quiet',
+                "datain",
+                "dataout"]
+        got = from_to(aiff,
+                      lame,
+                      logging=False)
+        self.assertEqual(got, mp3)
