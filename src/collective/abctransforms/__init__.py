f3ec900a64a7db913869067cd490527f65cf24e0
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-01-14 13:16:39 +0100

    add %abc magicNumbers in init

--- src/collective/abctransforms/__init__.py
+++ src/collective/abctransforms/__init__.py
@@ -15,4 +15,3 @@ if '%abc' not in values:
     logger.info(u'add %abc in magicNumbers')
     m = [0, 'string', '=', '%abc', 'text/vnd.abc']
     magicNumbers.append(magicTest(m[0], m[1], m[2], m[3], m[4]))
-
